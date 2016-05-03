11d9b34567ed4217fc75944c24619f0064aab106
Airan L Carreño <aleonardocarreno@unipanamericana.edu.co>
2016-05-03 15:54:39 -0500

    Cambios - Creacion de pagina Perfil - implementacion de clases para objetos perfil -Edicion y creacion de hojas de estilo - optimizacion de codigo

---
+++ src/java/ManagedBean/ChartEstadistica.java
@@ -0,0 +1,59 @@
+package ManagedBean;
+ 
+import javax.annotation.PostConstruct;
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import org.primefaces.model.chart.PieChartModel;
+ 
+@ManagedBean
+public class ChartEstadistica implements Serializable {
+ 
+    private PieChartModel pieModel1;
+    private PieChartModel pieModel2;
+ 
+    @PostConstruct
+    public void init() {
+        createPieModels();
+    }
+ 
+    public PieChartModel getPieModel1() {
+        return pieModel1;
+    }
+     
+    public PieChartModel getPieModel2() {
+        return pieModel2;
+    }
+     
+    private void createPieModels() {
+        createPieModel1();
+        createPieModel2();
+    }
+ 
+    private void createPieModel1() {
+        pieModel1 = new PieChartModel();
+         
+        pieModel1.set("Brand 1", 540);
+        pieModel1.set("Brand 2", 325);
+        pieModel1.set("Brand 3", 702);
+        pieModel1.set("Brand 4", 421);
+         
+        pieModel1.setTitle("Simple Pie");
+        pieModel1.setLegendPosition("w");
+    }
+     
+    private void createPieModel2() {
+        pieModel2 = new PieChartModel();
+         
+        pieModel2.set("Peso 1", 540);
+        pieModel2.set("Glicemia 2", 325);
+        pieModel2.set("Coresterol 3", 702);
+        pieModel2.set("Ritmo 4", 421);
+         
+        pieModel2.setTitle(" Datos Diagnostico Reporte ");
+        pieModel2.setLegendPosition("se");
+        pieModel2.setFill(false);
+        pieModel2.setShowDataLabels(true);
+        pieModel2.setDiameter(150);
+    }
+     
+}
